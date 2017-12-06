06436f510429d9b6c85912e43aa39009212e7e63
Olivia Gardella <gardella@umich.edu>
2017-12-05 20:25:34 -0500

    Found the days the interactions took place for both apis

--- project4.py
+++ project4.py
@@ -40,8 +40,6 @@ except:                                         #if the cache file doesn't exsit
 
 # Facebook api ------
 def fbapi(user_id):
-    #user_id = '943225772440871'
-    #FBresults = None
     fb_access_token = api_info.fb_access_token
     graph = facebook.GraphAPI(fb_access_token)
     posts = graph.get_connections(id='me', connection_name='posts')
@@ -76,21 +74,33 @@ def fbapi(user_id):
 # access exactly 100 interactions for the api; 100 of my posts (or less if I don't have enough posts)
 my_posts = fbapi('943225772440871')
 
-# find the days these interactions took place
+# find the days these interactions took place <---- ASK IF THIS BEING IN DATABASE IS OK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# x = my_posts[0]['created_time']
+# print(datetime.strptime(x, '%Y-%m-%dT%H:%M:%S+0000').weekday())
+# print(datetime.today().weekday())
+
+def day(x):
+    days = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
+    return days[x]
+# print(fbday(datetime.strptime(x, '%Y-%m-%dT%H:%M:%S+0000').weekday()))
 
 # write the data to the database
 conn = sqlite3.connect('FBDatabase.sqlite')
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Facebook')
-cur.execute('CREATE TABLE Facebook (message TEXT, story VARCHAR, created_time VARCHAR, id VARCHAR)')
+cur.execute('CREATE TABLE Facebook (message TEXT, story VARCHAR, created_time VARCHAR, created_day VARCHAR, id VARCHAR)')
 
 x = 0
 for post in my_posts:
     if x < 100:
     #print(post.keys())
-        cur.execute('INSERT INTO Facebook (message, story, created_time, id) VALUES (?,?,?,?)',
-        (post.get('message', 'none'), post.get('story', 'none'), post['created_time'], post['id']))
+        cur.execute('INSERT INTO Facebook (message, story, created_time, created_day, id) VALUES (?,?,?,?,?)',
+        (post.get('message', 'none'),
+        post.get('story', 'none'),
+        post['created_time'],
+        day(datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S+0000').weekday()),
+        post['id']))
         x += 1
 
 conn.commit()
@@ -123,21 +133,28 @@ def darkskyapi(lat_lng):
 # access exactly 100 interactions for the api; temperature for the next 100 hours in Ann Arbor
 aa_temps = darkskyapi('42.280841, -83.738115')
 
-# find the days these interactions took place???
+# find the days these interactions took place??? <-MAKE SURE THIS IS OK, GETTING THE DAY ITS HAPPENING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# x = aa_temps['hourly']['data'][0]['time']
+# print (x)
+# print(datetime.fromtimestamp(x).weekday())
+# print(datetime.fromtimestamp(aa_temps['hourly']['data'][86]['time']).weekday())
+# print(datetime.strptime(x, '%Y-%m-%dT%H:%M:%S+0000').weekday())
+# print(datetime.today().weekday())
 
 # write the data to the database
 conn = sqlite3.connect('DarkSkyDatabase.sqlite')                                #connect to sqlite and initate the file
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS DarkSky')                                     #if the darksky table already exists, get rid of it
-cur.execute('CREATE TABLE DarkSky (time VARCHAR, temperature VARCHAR)')         #create the DarkSky table with the following columns
+cur.execute('DROP TABLE IF EXISTS DarkSky')                                                  #if the darksky table already exists, get rid of it
+cur.execute('CREATE TABLE DarkSky (time VARCHAR, day VARCHAR, temperature VARCHAR)')         #create the DarkSky table with the following columns
 
 x = 0
 for hour in aa_temps['hourly']['data']:
     #print (hour['temperature'])
     if x < 100:
-        cur.execute('INSERT INTO DarkSky (time, temperature) VALUES (?,?)',
+        cur.execute('INSERT INTO DarkSky (time, day, temperature) VALUES (?,?,?)',
         (str(datetime.fromtimestamp(hour['time'])),                             #convert to datetime
+        day(datetime.fromtimestamp(hour['time']).weekday()),
         hour['temperature']))
         x += 1
 
